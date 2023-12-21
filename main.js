ede0cfe09ebbf712e828be8939b84ebb26fd636d
Mohammed Haydar <mr.hamood277@gmail.com>
2023-12-21 04:50:58 +0000

    bug fixed: now spaces will reflect to the initialLessons when the user add them from fetchedLessons (searched items)

--- main.js
+++ main.js
@@ -15,27 +15,46 @@ export default new Vue({
       name: '',
       phone: ''
     },
-    searchDelay: 1000,
+    LOAD_LESSONS_DELAY: 500,
+    SEARCH_DELAY: 1000,
     lastSearchQuery: ''
   },
   methods: {
-    // methods are not cached and are re-evaluated on every render
     addToCart: function (lesson) {
       if (lesson.spaces > 0) {
-        // only when there are spaces available then add to cart
+        /*
+         * only when there are spaces available then add to cart
+         * and update the spaces in the lesson
+         * Also update the initialLessons[] for that specific lesson
+         */
         lesson.spaces -= 1
         const existingLesson = this.cart.find(item => item._id === lesson._id)
+
         if (existingLesson) {
-          // we'll check the lesson by it's id, we'll compare it to the cart using find method, if its there then we'll update the values
+          /*
+           * we'll check the lesson by it's id, we'll compare it to the cart using find method,
+           * if its there then we'll update the values
+           */
           existingLesson.bookedClasses += 1
           existingLesson.totalPrice += lesson.price
         } else {
-          //if not then we'll creat a new object for it to the cart and  push it to the cart
+          //if not then we'll creat a new object for it to the cart and push it to the cart
           const cartLesson = Object.assign({}, lesson, {
             bookedClasses: 1,
             totalPrice: lesson.price
           })
           this.cart.push(cartLesson)
+        }
+
+        /*
+         * Update initialLessons array with the new  bookedClasses
+         * so that the user can see the updated spaces in the lessons list (Landing page)
+         */
+        const initialLessonIndex = this.initialLessons.findIndex(
+          item => item._id === lesson._id
+        )
+        if (initialLessonIndex !== -1) {
+          this.initialLessons[initialLessonIndex].spaces = lesson.spaces
         }
       }
     },
@@ -127,7 +146,7 @@ export default new Vue({
         fetch('http://localhost:5000/lessons')
           .then(res => res.json())
           .then(lessons => {
-            this.initialLessons = lessons.slice()
+            this.initialLessons = lessons.slice() // copy the lessons array to initialLessons
             this.fetchedLessons = lessons
           })
           .catch(error => alert(error))
@@ -135,13 +154,16 @@ export default new Vue({
             //loadingPage.remove()
             clearInterval(loadLessonsInterval)
           })
-      }, 4000)
+      }, this.LOAD_LESSONS_DELAY)
     },
 
     resetCart: function () {
-      //just reseting the array to empty array will reset it
       this.cart = []
-      this.loadLessons()
+      try {
+        this.loadLessons()
+      } catch (error) {
+        aler('Error resetting cart: ', error)
+      }
     },
 
     toggleSorting: function () {
@@ -206,7 +228,17 @@ export default new Vue({
             fetch(`http://localhost:5000/search?query=${encodeURIComponent(searchQuery)}`)
               .then(res => res.json())
               .then(lessons => {
-                this.fetchedLessons = lessons
+                this.fetchedLessons = lessons.map(lesson => {
+                  /*
+                   * if the lesson that is fetched was is already in the cart
+                   * then update the spaces with the bookedClasses
+                   */
+                  const cartLesson = this.cart.find(item => item._id === lesson._id)
+                  if (cartLesson) {
+                    lesson.spaces -= cartLesson.bookedClasses
+                  }
+                  return lesson
+                })
               })
               .catch(error => {
                 console.error('Error fetching search results:', error)
@@ -215,7 +247,7 @@ export default new Vue({
             this.fetchedLessons = this.initialLessons
           }
         }
-      }, this.searchDelay)
+      }, this.SEARCH_DELAY)
 
       /**
        *  Call the delayedFetch to make the fetch request when the user stops typing
